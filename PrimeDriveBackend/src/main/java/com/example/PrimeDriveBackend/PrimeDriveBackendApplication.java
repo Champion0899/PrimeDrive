4a918f6aa0eba579cdb5bb4a9fa586f6e9689ec2
Jamie Schüpbach <jsc140897@stud.gibb.ch>
2025-04-22 15:01:23 +0200

    backend

---
+++ PrimeDriveBackend/src/main/java/com/example/PrimeDriveBackend/PrimeDriveBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.PrimeDriveBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeDriveBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeDriveBackendApplication.class, args);
+	}
+
+}
---
+++ PrimeDriveBackend/src/test/java/com/example/PrimeDriveBackend/PrimeDriveBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PrimeDriveBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeDriveBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
