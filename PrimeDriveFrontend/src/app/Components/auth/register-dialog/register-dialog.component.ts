d5b42fe0a5566fc3c8f67a42d2a6f6d21bd23d4b
djredflame <djredflame@gmail.com>
2025-06-03 13:30:14 +0200

    feat: added register formular

--- PrimeDriveFrontend/src/app/Components/auth/register-dialog/register-dialog.component.ts
+++ PrimeDriveFrontend/src/app/Components/auth/register-dialog/register-dialog.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-register-dialog',
   standalone: true,
-  imports: [],
+  imports: [
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    FormsModule,
+  ],
   templateUrl: './register-dialog.component.html',
-  styleUrl: './register-dialog.component.scss'
+  styleUrl: './register-dialog.component.scss',
 })
 export class RegisterDialogComponent {
+  public username: string = '';
+  public password: string = '';
+  public confirmPassword: string = '';
+  public email: string = '';
+  public birthdate: string = '';
+  public address: string = '';
+  public zipCode: string = '';
+  public city: string = '';
+  public country: string = '';
+  public phoneNumber: string = '';
+  private role: string = 'user';
 
+  public register() {}
 }
