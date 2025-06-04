798a3dc9acdb05f344320f4f331a867ca2d83131
djredflame <djredflame@gmail.com>
2025-06-04 22:52:19 +0200

    feat: admin section

--- PrimeDriveFrontend/src/app/Web/admin/admin.component.ts
+++ PrimeDriveFrontend/src/app/Web/admin/admin.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [],
+  imports: [MatTabsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
   templateUrl: './admin.component.html',
-  styleUrl: './admin.component.scss'
+  styleUrl: './admin.component.scss',
 })
-export class AdminComponent {
-
-}
+export class AdminComponent {}
